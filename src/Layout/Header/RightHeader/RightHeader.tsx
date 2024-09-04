402255be73b53881e5b37c0745d90b5cd6e5e4a0
pip <phamlehuy52@gmail.com>
2024-09-04 16:03:28 +0700

    remove redundant

--- src/Layout/Header/RightHeader/RightHeader.tsx
+++ src/Layout/Header/RightHeader/RightHeader.tsx
@@ -1,17 +1,20 @@
 import { Col } from "reactstrap";
 import { UL } from "../../../AbstractElements";
 import UserProfile from "./UserProfile/UserProfile";
-import ZoomInOut from "./ZoomInOut/ZoomInOut";
 
 const RightHeader = () => {
-    return (
-        <Col xl="3" xs="3" className="nav-right pull-right right-header p-0 ms-auto">
-            <UL className="nav-menus simple-list flex-row">
-                <ZoomInOut />
-                <UserProfile />
-            </UL>
-        </Col>
-    );
+  return (
+    <Col
+      xl="3"
+      xs="3"
+      className="nav-right pull-right right-header p-0 ms-auto"
+    >
+      <UL className="nav-menus simple-list flex-row">
+        {/* <ZoomInOut /> */}
+        <UserProfile />
+      </UL>
+    </Col>
+  );
 };
 
 export default RightHeader;
--- src/Layout/Header/RightHeader/UserProfile/UserProfile.tsx
+++ src/Layout/Header/RightHeader/UserProfile/UserProfile.tsx
@@ -3,21 +3,27 @@ import { dynamicImage } from "../../../../Service";
 import ProfileBox from "./ProfileBox";
 
 const UserProfile = () => {
-    return (
-        <LI className="profile-nav onhover-dropdown">
-            <div className="media profile-media">
-                <Image className="b-r-10" src={dynamicImage("/assets/images/logo.png")} alt="image" />
-                <div className="media-body d-xxl-block d-none box-col-none">
-                    <div className="d-flex align-items-center gap-2">
-                        <span style={{color: "#066"}}>TQ24</span>
-                        <i className="middle fa fa-angle-down"></i>
-                    </div>
-                    <P className="mb-0 font-roboto" style={{color: '#066'}}>Admin</P>
-                </div>
-            </div>
-            <ProfileBox />
-        </LI>
-    );
+  return (
+    <LI className="profile-nav onhover-dropdown">
+      <div className="media profile-media">
+        <Image
+          className="b-r-10"
+          src={dynamicImage("/assets/images/logo_trang.png")}
+          alt="image"
+        />
+        <div className="media-body d-xxl-block d-none box-col-none">
+          <div className="d-flex align-items-center gap-2">
+            <span style={{ color: "#066" }}>TQ24</span>
+            <i className="middle fa fa-angle-down"></i>
+          </div>
+          <P className="mb-0 font-roboto" style={{ color: "#066" }}>
+            Admin
+          </P>
+        </div>
+      </div>
+      <ProfileBox />
+    </LI>
+  );
 };
 
 export default UserProfile;
