9158248f28ce332eaaff95324cdec37f6da74b1d
pip <phamlehuy52@gmail.com>
2024-09-04 16:33:16 +0700

    remove redundant

--- src/Layout/Sidebar/LogoWrapper.tsx
+++ src/Layout/Sidebar/LogoWrapper.tsx
@@ -1,39 +1,44 @@
 import { Link } from "react-router-dom";
-import { FeatherIcons, Image, SVG } from "../../AbstractElements";
+import { Image } from "../../AbstractElements";
 import { dynamicImage } from "../../Service";
-import { useThemeStore } from "../../store/theme";
 import { useLayoutStore } from "../../store/layout";
+import { useThemeStore } from "../../store/theme";
 
 const LogoWrapper = () => {
-    const { toggleSidebar, setToggleSidebar, handleResponsiveToggle } = useLayoutStore();
-    const { sidebarIconType } = useThemeStore();
+  const { toggleSidebar, setToggleSidebar, handleResponsiveToggle } =
+    useLayoutStore();
+  const { sidebarIconType } = useThemeStore();
 
-    return (
-        <>
-            <div className="logo-wrapper m-b-30 p-0 d-flex justify-content-center">
-                <Link to={`/sport/list`} className="d-inline-block ">
-                    <Image
-                        className="img-fluid "
-                        src={dynamicImage("/assets/images/logo_trang.png")}
-                        height={70}
-                        width={140}
-                        alt="logo"
-                    />
-                </Link>
-                <div className="back-btn" onClick={() => handleResponsiveToggle()}>
-                    <i className="fa fa-angle-left"></i>
-                </div>
-                {/* <div className="toggle-sidebar" onClick={() => setToggleSidebar(!toggleSidebar)}>
+  return (
+    <>
+      <div className="logo-wrapper m-b-30 p-0 d-flex justify-content-center">
+        <Link to={`/sport/list`} className="d-inline-block ">
+          <Image
+            className="img-fluid "
+            src={dynamicImage("/assets/images/logo_trang.png")}
+            height={70}
+            width={140}
+            alt="logo"
+          />
+        </Link>
+        <div className="back-btn" onClick={() => handleResponsiveToggle()}>
+          <i className="fa fa-angle-left"></i>
+        </div>
+        {/* <div className="toggle-sidebar" onClick={() => setToggleSidebar(!toggleSidebar)}>
                     <FeatherIcons className="status_toggle middle sidebar-toggle" iconName="Grid" />
                 </div> */}
-            </div>
-            <div className="logo-icon-wrapper">
-                <Link to={`/dashboard`}>
-                    <Image className="img-fluid" src={dynamicImage("logo/logo-icon.png")} alt="logo" />
-                </Link>
-            </div>
-        </>
-    );
+      </div>
+      <div className="logo-icon-wrapper">
+        <Link to={`/dashboard`}>
+          <Image
+            className="img-fluid"
+            src={dynamicImage("logo/logo-icon.png")}
+            alt="logo"
+          />
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default LogoWrapper;
