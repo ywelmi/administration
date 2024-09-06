0beb92161ee36014a5cfad99fe22b60946cbb226
ywelmi <quangminhle2710@gmail.com>
2024-09-06 08:53:11 +0700

    update 06/9

--- src/Routes/Route.tsx
+++ src/Routes/Route.tsx
@@ -12,82 +12,83 @@ import { PageTablequalifyingKnockout } from "../Pages/TablequalifyingKnockout/Li
 import { PageTablequalifyingMatch } from "../Pages/TablequalifyingMatch/ListTablequalifyingMatch";
 import { PageTablequalifyingMatchReport } from "../Pages/TableQualifyingMatchReport/ListMatchReport";
 import { PageTeam } from "../Pages/Team/ListTeam";
+import { PageTeamNew } from "../Pages/Team/ListTeamNew";
 import { PageTeammember } from "../Pages/Teammember/ListTeammember";
 import { PageUser } from "../Pages/User/ListUser";
 
 const Routes = [
-  {
-    path: "/dashboard",
-    Component: <PageSport />,
-  },
-  {
-    path: "/user/list",
-    Component: <PageUser />,
-  },
-  {
-    path: "/org/list",
-    Component: <ListOrg />,
-  },
-  {
-    path: "/sport/list",
-    Component: <PageSport />,
-  },
-  {
-    path: "/sport/register",
-    Component: <PageCompetitionRegister />,
-  },
-  {
-    path: "/teammember/list",
-    Component: <PageTeammember />,
-  },
-  {
-    path: "/team/list",
-    Component: <PageTeam />,
-  },
-  {
-    path: "/tablequalifyings/list/:sport_id?",
-    Component: <PageTablequalifying />,
-  },
-  {
-    path: "/tablequalifyings/match/:table_id?",
-    Component: <PageTablequalifyingMatch />,
-  },
-  {
-    path: "/tablequalifyings/match-report/:table_id?",
-    Component: <PageTablequalifyingMatchReport />,
-  },
-  {
-    path: "/tablequalifyings/knockout/:sport_id",
-    Component: <PageTablequalifyingKnockout />,
-  },
-  {
-    path: "/tablequalifyings/knockout",
-    Component: <PageTablequalifyingKnockout />,
-  },
-  {
-    path: "/lotsdraw/list/:sport_id?",
-    Component: <PageLotsDraw1 />,
-  },
-  {
-    path: "/martialart/list",
-    Component: <PageMartialArt />,
-  },
-  {
-    path: "/martialart/:sport_id/knockout/:content_id",
-    Component: <MartialArtKnockout />,
-  },
-  {
-    path: "/martialartmilitia",
-    Component: <PageMartialArtMilitia />,
-  },
-  {
-    path: "/resultexport",
-    Component: <PageResultExport />,
-  },
-  {
-    path: "/reportresult",
-    Component: <PageReportResult />,
-  },
+    {
+        path: "/dashboard",
+        Component: <PageSport />,
+    },
+    {
+        path: "/user/list",
+        Component: <PageUser />,
+    },
+    {
+        path: "/org/list",
+        Component: <ListOrg />,
+    },
+    {
+        path: "/sport/list",
+        Component: <PageSport />,
+    },
+    {
+        path: "/sport/register",
+        Component: <PageCompetitionRegister />,
+    },
+    {
+        path: "/teammember/list",
+        Component: <PageTeammember />,
+    },
+    {
+        path: "/team/list",
+        Component: <PageTeamNew />,
+    },
+    {
+        path: "/tablequalifyings/list/:sport_id?",
+        Component: <PageTablequalifying />,
+    },
+    {
+        path: "/tablequalifyings/match/:table_id?",
+        Component: <PageTablequalifyingMatch />,
+    },
+    {
+        path: "/tablequalifyings/match-report/:table_id?",
+        Component: <PageTablequalifyingMatchReport />,
+    },
+    {
+        path: "/tablequalifyings/knockout/:sport_id",
+        Component: <PageTablequalifyingKnockout />,
+    },
+    {
+        path: "/tablequalifyings/knockout",
+        Component: <PageTablequalifyingKnockout />,
+    },
+    {
+        path: "/lotsdraw/list/:sport_id?",
+        Component: <PageLotsDraw1 />,
+    },
+    {
+        path: "/martialart/list",
+        Component: <PageMartialArt />,
+    },
+    {
+        path: "/martialart/:sport_id/knockout/:content_id",
+        Component: <MartialArtKnockout />,
+    },
+    {
+        path: "/martialartmilitia",
+        Component: <PageMartialArtMilitia />,
+    },
+    {
+        path: "/resultexport",
+        Component: <PageResultExport />,
+    },
+    {
+        path: "/reportresult",
+        Component: <PageReportResult />,
+    },
 ];
 
 export default Routes;
