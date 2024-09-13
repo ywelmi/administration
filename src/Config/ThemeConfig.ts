13072ef2827d0145c5eacfa1a492f6a8dbb70dcd
pip <phamlehuy52@gmail.com>
2024-09-13 09:51:24 +0700

    remove svg

---
+++ src/Config/ThemeConfig.ts
@@ -0,0 +1,18 @@
+export class ConfigDB {
+    static data = {
+      settings: {
+        layout_type: 'ltr',
+        layout_class: 'compact-wrapper',
+        sidebar: {
+          iconType: 'stroke',
+        },
+        sidebar_setting: 'default-sidebar',
+      },
+      color: {
+        primary_color:'#006666',
+        secondary_color: '#FE6A49',
+        mix_background_layout: 'light-only',
+      }
+    };
+  }
+  export default ConfigDB;
